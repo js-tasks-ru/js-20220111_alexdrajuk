96e512fdf089f5d64724f620d0dbc1442dca722e
Oleksandr Draiuk <alexdrajuk@gmail.com>
2022-01-15 01:47:30 +0200

    Solution for02-javascript-data-types 1-sort-strings

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -1,3 +1,12 @@
+function compareFn(a, b) {
+    const options = {
+        caseFirst: 'upper',
+        sensitivity: 'variant'
+    };
+    const collator = new Intl.Collator(['ru', 'en'], options); 
+    return collator.compare(a, b);
+}
+
 /**
  * sortStrings - sorts array of string by two criteria "asc" or "desc"
  * @param {string[]} arr - the array of strings
@@ -5,5 +14,10 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
-
+    const arrCopy = arr.slice(0);
+    arrCopy.sort(compareFn);
+    if (param === 'desc') {
+        arrCopy.reverse();
+    }
+    return arrCopy;
 }
