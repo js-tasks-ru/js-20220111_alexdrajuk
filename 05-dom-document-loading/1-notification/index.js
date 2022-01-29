c3042178c625ea01ec4adb4550be1fe502da9b6c
Oleksandr Draiuk <alexdrajuk@gmail.com>
2022-01-30 01:32:01 +0200

    solution for 05-dom-document-loading 1-notification

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,50 @@
 export default class NotificationMessage {
+    static notification;
+    constructor(message = '', { duration = 2000, type = 'success' } = {}) {
+        this.message = message;
+        this.duration = duration;
+        this.type = type;
+        this.show();
+    }
 
+    getTemplate() {
+        return `
+            <div class="notification ${this.type}" style="--value:${Math.round(this.duration / 1000)}s">
+                <div class="timer"></div>
+                <div class="inner-wrapper">
+                    <div class="notification-header">${this.type}</div>
+                    <div class="notification-body">
+                        ${this.message}
+                    </div>
+                </div>
+            </div>
+        `;
+    }
+
+    show(parent = document.body) {
+        if (NotificationMessage.notification) {
+            NotificationMessage.notification.remove();
+        }
+        const element = document.createElement('div');
+        element.innerHTML = this.getTemplate();
+        this.element = element.firstElementChild;
+        parent.append(this.element);
+        NotificationMessage.notification = this.element;
+        
+        setTimeout(() => {
+            this.remove();
+        }, this.duration);
+    }
+
+    remove() {
+        if (this.element) {
+            this.element.remove();
+        }
+    }
+
+    destroy() {
+        this.remove();
+        this.element = null;
+        NotificationMessage.notification = null;
+    }
 }
